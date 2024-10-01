98c9f75e636d5cbb96d0e2d21eae79bc8ef879aa
EdwartLeal <143855239+EdwartLeal@users.noreply.github.com>
2024-10-01 11:43:06 -0300

    Add files via upload

---
+++ atividade 05.py
@@ -0,0 +1,15 @@
+from array import array
+
+array1 = [0]*10
+array2 = [0]*10
+tam = len(array1)
+
+for x in range(tam)
+    array1[x]=int(input("digite um numero"))
+    mult = int(input("digite o multiplicador"))
+for i in range(tam)
+    array2[x]=array1[i]*mult
+print(array1)
+print(mult)
+print(array2)
+
---
+++ atv.py
@@ -0,0 +1,5 @@
+"""variavel simples aceita apenas 1 informacao
+ja a composta aceita mais informacoes"""
+arrayCompras = ['banana','laranja','maça']
+for i in arrayCompras:
+    print(i)
